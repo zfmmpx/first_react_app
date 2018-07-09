a8013b8895894b172893bf97f39aa3a3d7a7df96
unknown <huangjian@ways.cn>
2018-07-09 18:34:25 +0800

    公司0709

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClickCounter from './ClickCounter';
+import './index.css';
+
+ReactDOM.render(
+    <ClickCounter />,
+    document.getElementById('root')
+);
+
